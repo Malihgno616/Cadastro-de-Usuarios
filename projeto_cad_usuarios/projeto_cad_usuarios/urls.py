eb657dafd902806dfde21d78f784cc5ab1bccf45
ackindev <ackinlino123@gmail.com>
2024-09-13 08:36:54 -0300

    Inicio Porjeto Django

---
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from app_cad_usuarios import views
+
+
+urlpatterns = [
+    # rota, view responsavel, nome da referência
+    #usuarios.com
+    path('',views.home,name='home'),
+
+]
